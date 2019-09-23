91e238551f66d7d275fc2973ff08bce01de687c8
JingshengYe <jingsheng-ye@msyc.cc>
2019-09-23 16:11:38 +0800

    :wrench: 修改错误日志打印导致服务停止问题

--- MalAnimeController.go
+++ MalAnimeController.go
@@ -21,7 +21,7 @@ func Search(c echo.Context) error {
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(malAnimeServiceAddress, grpc.WithInsecure())
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		log.Printf("did not connect: %s", err)
 		return c.JSON(http.StatusBadRequest, "connect service error")
 	}
 	defer conn.Close()
@@ -31,7 +31,7 @@ func Search(c echo.Context) error {
 	defer cancel()
 	resp, err := client.Search(ctx, &animeProto.AnimeSearchRequest{Name: req.Name, Text: req.Text})
 	if err != nil {
-		log.Fatalf("error when search mal anime: %v", err)
+		log.Printf("error when search mal anime: %s", err)
 		return c.JSON(http.StatusBadRequest, "error when search mal anime")
 	}
 	log.Printf("search result: %s", resp)
